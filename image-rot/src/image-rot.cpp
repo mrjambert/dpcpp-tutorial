19e16acee4bc2ef2a6f4c92819c23e25fe419ad5
mrjambert <79665732+mrjambert@users.noreply.github.com>
2022-04-10 15:12:06 -0400

    Update image-rot.cpp

--- image-rot/src/image-rot.cpp
+++ image-rot/src/image-rot.cpp
@@ -91,8 +91,7 @@ void ImageConv_v1(queue &q, float *image_in, float *image_out, float sinTheta,
 
         float xpos = ((float)ix)*cosTheta + ((float)iy)*sinTheta;
         float ypos = -1.0f*((float)ix)*sinTheta + ((float)iy)*cosTheta;
-        if(((int)xpos >= 0) && ((int)xpos < ImageRows) &&
-        ((int)ypos >= 0) && ((int)ypos < ImageCols) )
+        //if(((int)xpos >= 0) && ((int)xpos < ImageRows) && ((int)ypos >= 0) && ((int)ypos < ImageCols) )
         dstPtr[(int)ypos * ImageRows + (int)xpos] = srcPtr[iy*ImageRows+ix];
 
       });
