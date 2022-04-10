4eb8ff54e3549fdaf24f364870b3a3eb343df90e
mrjambert <79665732+mrjambert@users.noreply.github.com>
2022-04-10 16:48:32 -0400

    Delete image-rot/build2 directory

--- image-rot/build2/src/CMakeFiles/fpga_profile.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/fpga_profile"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/fpga_profile.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
